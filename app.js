3d7294c54d6b0edfc89afc7636f019895b7a22ab
Justin Westmoreland <justinrwestmoreland@gmail.com>
2021-04-10 12:57:08 -0600

    handlebars and index files

---
+++ app.js
@@ -0,0 +1,8 @@
+function initMap() {
+    // Create the map.
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: { lat: 39.7559, lng: -104.9942 },
+    });
+
+}
