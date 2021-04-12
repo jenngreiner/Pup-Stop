70de11b65eabee2abd366d3593d4c0ef1bf9e96d
GQuak <75180482+GQuak@users.noreply.github.com>
2021-04-12 17:09:41 -0600

    encrypting seed passwords

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,6 +8,7 @@ const sequelize = require("../config/connection");
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log("\n----- DATABASE SYNCED -----\n");
+
   await seedUsers();
   console.log("\n----- USERS SEEDED -----\n");
 
